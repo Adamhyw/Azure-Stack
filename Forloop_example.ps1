99193aaa39c38d08fde9ef9b9bb0861e5544cf0a
Adamhyw <adamhyw@163.com>
2019-03-18 10:49:19 +0800

    Update Forloop_example.ps1

--- Forloop_example.ps1
+++ Forloop_example.ps1
@@ -1,6 +1,6 @@
 for($j = 2; $j -le 8; $j++)
         {
             $VmRgName = "vmrg" + $j
-            Remove-AzureRmResourceGroup -Name $VmRgName 
+            Remove-AzureRmResourceGroup -Name $VmRgName -force
            
         }
